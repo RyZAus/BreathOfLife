1e8a8ba69ed967af1d3d3a5bd2dcb411e8afa935
Riley McGowan <1017093@student.sae.edu.au>
2021-10-09 13:52:48 +1100

    Colour Transition Working

---
+++ Assets/IndividualWork/RileyMcGowan/Scripts/ItemShaderTransition.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemShaderTransition : MonoBehaviour
+{
+    //Private Vars
+    private Renderer thisRenderer;
+    private float swapAmount;
+    private bool swapColour;
+    //Public Vars
+    [Tooltip("This is how long to swap materials for. Default 3.")]
+    public float timeToSwap = 3;
+    [Tooltip("Starting material.")]
+    public Material firstMaterial;
+    [Tooltip("End material.")]
+    public Material secondMaterial;
+
+    private void Awake()
+    {
+        //We are not swapping colour
+        swapColour = false;
+        //Grab the renderer
+        thisRenderer = GetComponent<Renderer>();
+        thisRenderer.material = firstMaterial;
+        //HACK This must be removed for outside function
+        ChangeColour();
+    }
+    
+    //Used for outside objects
+    public void ChangeColour()
+    {
+        //We are now swapping colour
+        swapColour = true;
+    }
+
+    private void Update()
+    {
+        if (swapColour == true)
+        {
+            //If we aren't finished swapping
+            if (swapAmount != 1)
+            {
+                //Swap the colour slowly
+                swapAmount = Mathf.Lerp(0, 1, (Time.time) / timeToSwap);
+                thisRenderer.material.Lerp(firstMaterial, secondMaterial, swapAmount);
+            }
+        }
+    }
+}
