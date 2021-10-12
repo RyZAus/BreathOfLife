e32f0685bcebd10fb35d6d603e4958dde1212f3a
Damien Harwood <1019243@student.sae.edu.au>
2021-10-12 21:19:05 +1100

    Trigger Wall Base

---
+++ Assets/IndividualWork/Damien Harwood/Scripts/TriggerWall.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DamienHarwood
+{
+    public class TriggerWall : MonoBehaviour
+    {
+        private GameObject _gameObject;
+        private Vector3 endPos;
+        private Vector3 startPos;
+        private Vector3 objPos;
+        public float moveDuration;
+        private float elapsedTime;
+        private float percentComplete;
+
+
+        public GameObject endPosMarker;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            GetPositions();
+        }
+
+        private void GetPositions()
+        {
+            // Sets the start position for the current move cycle to the current position and gets the position of the end marker
+            startPos = transform.position;
+            endPos = endPosMarker.transform.position;
+        }
+
+
+        // Update is called once per frame
+        void Update()
+        {
+            elapsedTime += Time.deltaTime;
+            float percentComplete = elapsedTime / moveDuration;
+            transform.position = Vector3.Lerp(startPos, endPos, percentComplete);
+        }
+
+        void TryMove()
+        {
+            
+        }
+    }
+}
